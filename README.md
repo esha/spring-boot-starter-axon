b73f880187c3d73dbaec6bbba16e9c6f4c8aad16
Christopher Elkins <chrise@esha.com>
2014-09-05 19:45:09 -0700

    Initial project import

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-spring-boot-starter-axon
+Spring Boot Axon Starter
 ========================
 
-Spring Boot Axon Starter
+[![Build Status](https://travis-ci.org/esha/spring-boot-starter-axon.svg?branch=master)](https://travis-ci.org/esha/spring-boot-starter-axon)
---
+++ src/test/java/com/esha/spring/boot/axon/AxonAutoConfigurationTests.java
@@ -0,0 +1,73 @@
+package com.esha.spring.boot.axon;
+
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.eventhandling.EventListener;
+import org.axonframework.eventhandling.annotation.EventHandler;
+import org.axonframework.eventstore.EventStore;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import static org.junit.Assert.assertNotNull;
+
+public class AxonAutoConfigurationTests {
+
+    private AnnotationConfigApplicationContext context;
+
+    @Before
+    public void setUp() {
+        this.context = new AnnotationConfigApplicationContext();
+    }
+
+    @After
+    public void tearDown() {
+        if (this.context != null) {
+            this.context.close();
+        }
+    }
+
+    @Test
+    public void autoConfigured() throws Exception {
+        this.context.register(AxonAutoConfiguration.class);
+        this.context.refresh();
+        assertNotNull(this.context.getBean(CommandBus.class));
+        assertNotNull(this.context.getBean(CommandGateway.class));
+        assertNotNull(this.context.getBean(EventBus.class));
+        assertNotNull(this.context.getBean(EventStore.class));
+    }
+
+    @Test
+    public void generatedHandlerAdapters() throws Exception {
+        this.context.register(AxonAutoConfiguration.class);
+        this.context.register(TestConfiguration.class);
+        this.context.refresh();
+        assertNotNull(this.context.getBean(CommandHandler.class));
+        assertNotNull(this.context.getBean(EventListener.class));
+    }
+
+    @Configuration
+    protected static class TestConfiguration {
+
+        protected static class TestCommand {}
+
+        protected static class TestEvent {}
+
+        @Component
+        protected static class TestHandlers {
+
+            @org.axonframework.commandhandling.annotation.CommandHandler
+            public void handleCommand(TestCommand command) {}
+
+            @EventHandler
+            public void handleEvent(TestEvent event) {}
+        }
+    }
+
+}
